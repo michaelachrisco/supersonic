a5824637929e0b8d768fbed918b2b8ac8e17c8a8
dphaener <dphaener@gmail.com>
2016-03-08 08:04:33 -0800

    Expose database adapter

--- core/supersonic.js
+++ core/supersonic.js
@@ -4,6 +4,7 @@ import BaseModel from './model/base_model'
 import ApplicationMiddleware from './server/middleware/application'
 import NetworkLayer from './server/network_layer'
 import Router from './server/router'
+import DatabaseAdapter from './model/database_adapter'
 
 export class Supersonic {
   static Application = {}
@@ -20,5 +21,6 @@ export {
   BaseModel,
   ApplicationMiddleware,
   NetworkLayer,
-  Router
+  Router,
+  DatabaseAdapter
 }
